5c8e8ca463150657b9ba243086860bd82faf5bbc
DavidAntonioO <david-antonio.olmos@atos.net>
2022-02-11 17:17:54 -0800

    Lesson 3

--- app.js
+++ app.js
@@ -1,3 +1,8 @@
-var banner = document.getElementById('page-banner')
+const titles = document.getElementsByClassName('title');
 
-var bookList = document.getElementById('book-list')
+console.log(Array.isArray(titles));
+console.log(Array.isArray(Array.from(titles)));
+
+Array.from(titles).forEach(function(title){
+  console.log(title);
+});
